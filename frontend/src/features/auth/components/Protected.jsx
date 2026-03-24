dc0c461e2a4b07a241b97f986d70f6335eba4b3c
shani-tiwari <shanitiwari2021@gmail.com>
2026-03-24 22:31:39 +0530

    protected page - done

--- frontend/src/features/auth/components/Protected.jsx
+++ frontend/src/features/auth/components/Protected.jsx
@@ -1,6 +1,27 @@
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router";
 
-export default function Protected() {
-  return (
-    <div>Protected</div>
-  )
+
+/**
+ * @name Protected - component for protecting routes
+ * @description Protects routes from unauthorized access
+ * @param {React.ReactNode} children - The component to render if the user is authenticated
+ * @returns {React.ReactNode} The component to render if the user is authenticated
+ */
+export default function Protected({children}) {
+
+    const { loading, user } = useAuth();
+    const navigate = useNavigate();
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+
+    if(!user){
+        return navigate('/login');
+    }
+
+    return(
+        {children}
+    );
 }
