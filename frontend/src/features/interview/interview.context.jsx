591e4756ecd364123e8ed807245e4e53ad193b77
shani-tiwari <shanitiwari2021@gmail.com>
2026-03-29 21:44:59 +0530

    states naming fix

--- frontend/src/features/interview/interview.context.jsx
+++ frontend/src/features/interview/interview.context.jsx
@@ -5,13 +5,13 @@ export const InterviewContext = createContext();
 
 export const InterviewContextProvider = ({ children }) => {
 
-    const [load, setLoad] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [report, setReport] = useState(null);  
     const [reports, setReports] = useState([]);  
 
 
     return (
-        <InterviewContext.Provider value={{ load, report, setLoad, setReport, reports, setReports }}>
+        <InterviewContext.Provider value={{ loading, report, setLoading, setReport, reports, setReports }}>
             {children}
         </InterviewContext.Provider>
     );
